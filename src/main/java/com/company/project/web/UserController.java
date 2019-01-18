a91f6f25c86776e9e9bdb2e124131ee6bb6a54d7
yangkai <yangkai@towantto.com>
2019-01-18 12:37:15 +0800

    添加用户接口

---
+++ src/main/java/com/company/project/web/UserController.java
@@ -0,0 +1,60 @@
+package com.company.project.web;
+import com.company.project.core.Result;
+import com.company.project.core.ResultGenerator;
+import com.company.project.model.User;
+import com.company.project.service.UserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+* Created by CodeGenerator on 2019/01/07.
+*/
+@RestController
+@RequestMapping("/api/newcapec/")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @PostMapping("doors/status")
+    public Result<Map<String,String>>add(@RequestParam HashMap<String,String> map) {
+        Result result = ResultGenerator.genSuccessResult();
+        result.setData(map);
+        return result;
+    }
+
+    @PostMapping("/delete")
+    public Result delete(@RequestParam Integer id) {
+        userService.deleteById(id);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/update")
+    public Result update(User user) {
+        userService.update(user);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/detail")
+    public Result detail(@RequestParam Integer id) {
+        User user = userService.findById(id);
+        return ResultGenerator.genSuccessResult(user);
+    }
+
+    @PostMapping("/list")
+    public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+        PageHelper.startPage(page, size);
+        List<User> list = userService.findAll();
+        PageInfo pageInfo = new PageInfo(list);
+        return ResultGenerator.genSuccessResult(pageInfo);
+    }
+}
